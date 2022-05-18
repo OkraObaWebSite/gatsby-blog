5e49dfbe10f7711f33cf5d506766139207f91f1d
OkraObaWebSite <105102954+OkraObaWebSite@users.noreply.github.com>
2022-05-17 22:33:59 -0700

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby + Netlify CMS Starter",
+    title: "OkraOba Art Portfolio Blog",
     description:
       "Checkout this art by OkraOba.",
   },
