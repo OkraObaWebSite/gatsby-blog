ffa213741742edd8333e1d9b4f79bcf54d315d74
OkraObaWebSite <105102954+OkraObaWebSite@users.noreply.github.com>
2022-05-17 22:10:11 -0700

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,8 +2,7 @@ module.exports = {
   siteMetadata: {
     title: "OkraOba Art Portfolio Blog",
     description:
-      "This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.",
-  },
+      "Check out this Art by OkraOba"
   plugins: [
     "gatsby-plugin-react-helmet",
     {
